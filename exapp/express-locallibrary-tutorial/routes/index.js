0b888a478859f3a563e5b4097f681a11473ac277
baljnyam <baljnyam@gmail.com>
2020-01-08 15:20:42 +1100

    some changes

---
+++ exapp/express-locallibrary-tutorial/routes/index.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+module.exports = router;
+
+//Import the mongoose module
+var mongoose = require("mongoose");
+
+//Set up default mongoose connection
+var mongoDB = "mongodb://127.0.0.1/my_database";
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+
+//Get the default connection
+var db = mongoose.connection;
+
+//Bind connection to error event (to get notification of connection errors)
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
